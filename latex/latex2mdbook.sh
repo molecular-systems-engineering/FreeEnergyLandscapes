d8e61cab2a5d9f93455e41fc83d6cbbd330a5aaf
Marcello Sega <m.sega@ucl.ac.uk>
2025-12-09 22:57:28 +0000

    fix bib 2

--- latex/latex2mdbook.sh
+++ latex/latex2mdbook.sh
@@ -6,5 +6,5 @@ fi
 out=`echo $1 | sed 's/\.tex/\.md/'`
 #pandoc $1 --from=latex --to=gfm+tex_math_dollars --citeproc  --lua-filter=`dirname $0`/fix_mdbook_refs.lua -o $out
 #pandoc $1 --from=latex --to=gfm+tex_math_dollars --citeproc  -o - | `dirname $0`/postprocess_refs.py > $out
-pandoc $1 --from=latex --to=markdown+tex_math_double_backslash   --lua-filter=`dirname $0`/math_fix.lua  --citeproc  -o - | `dirname $0`/convert_dollars.sh  | `dirname $0`/postprocess_refs.py |  pandoc --from=markdown+citations --to=markdown --citeproc --bibliography=`dirname $1`/../additional.bib -o - |  pandoc --from=markdown+citations --to=markdown --bibliography=`dirname $1`/../zotero.bib > $out
+pandoc $1 --from=latex --to=markdown+tex_math_double_backslash   --lua-filter=`dirname $0`/math_fix.lua  --citeproc   --bibliography=`dirname $1`/../additional.bib --bibliography=`dirname $1`/../zotero.bib | `dirname $0`/convert_dollars.sh  | `dirname $0`/postprocess_refs.py |  pandoc --from=markdown+citations --to=markdown --citeproc --bibliography=`dirname $1`/../additional.bib  --bibliography=`dirname $1`/../zotero.bib  > $out
 
